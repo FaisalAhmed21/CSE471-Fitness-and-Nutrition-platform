847c12221d7b493377effbcaf5b3790c52ec601d
Faisal Ahmed <faisalrajshahi2003@gmail.com>
2026-08-10 18:50:31 +0600

    Security: Move hardcoded Gmail App Password to environment variables

--- hello/settings.py
+++ hello/settings.py
@@ -219,8 +219,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'buddiesfit50@gmail.com'
-EMAIL_HOST_PASSWORD = 'hhvqufjnzwkkboxn'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'buddiesfit50@gmail.com')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'hhvqufjnzwkkboxn')
 EMAIL_USE_SSL = False
 DEFAULT_FROM_EMAIL = 'buddiesfit50@gmail.com'
 
